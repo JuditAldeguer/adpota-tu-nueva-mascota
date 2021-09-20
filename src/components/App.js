d9f1e8bc821c13a7382d1085178f6859b80590f9
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-20 21:05:59 +0200

    initial sturcture

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import '../styles/App.scss';
+import { useState } from 'react';
+import logoPatas from '../images/icono-patas.jpeg';
 
 function App() {
+  //estados
+  const [] = useState('');
+
+  //funciones
+
+  //return
   return (
     <div>
-      <header>
-        <h1 className="title">Starter Kit</h1>
+      <header className="header">
+        <h1 className="header--title">
+          Encuentra tu mitad perruna{' '}
+          <img className="header--img" src={logoPatas} alt="pata" />
+        </h1>
+        <menu>{/* hamburguesa */}</menu>
       </header>
+      <main>contenido</main>
     </div>
   );
 }
