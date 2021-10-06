537588cfd2b807265425617dd20e1a1f41881d26
JuditAldeguer <jud.vicens@hotmail.es>
2021-10-06 13:15:10 +0200

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,51 @@
 //Styless
 import '../styles/layout/header.scss';
 import perrito from '../images/perrito.png';
+import esFlag from '../images/es.png';
+import caFlag from '../images/ca.png';
+import enFlag from '../images/en.png';
 
 const Header = (props) => {
   return (
     <header className="header">
       <div className="header--div">
-        <img className="header--img__lang" src={perrito} alt="pata" />
+        <nav>
+          <ul>
+            <li className="language_es ">
+              <a href="">
+                {/* pendiente */}
+                <span className="language_flag">
+                  <img title="Español" src={esFlag} alt="Español" />
+                </span>
+                <span className="language_native span">Español</span>
+                <span className="language_translated span">Spanish</span>
+                <span className="language_code span">es</span>
+              </a>
+            </li>
+            <li className="language_ca">
+              <a href="">
+                {/* pendiente */}
+                <span className="language_flag">
+                  <img title="Català" src={caFlag} alt="Català" />
+                </span>
+                <span className="language_native span">Català</span>
+                <span className="language_translated span">Català</span>
+                <span className="language_code span">ca</span>
+              </a>
+            </li>
+            <li className="language_en ">
+              <a href="">
+                {/* pendiente */}
+                <span className="language_flag">
+                  <img title="English" src={enFlag} alt="English" />
+                </span>{' '}
+                <span className="language_native span">English</span>
+                <span className="language_translated span">English</span>
+                <span className="language_code span">en</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
       <div className="header--div div2">
         <img className="header--img" src={perrito} alt="pata" />
